b719cecc2e79a91e2245c29dea8e12210b0c65dd
Kai Wetlesen <kaiw@semiotic.ai>
2022-07-29 14:29:01 -0700

    Set up source amalgamator to accept warnings as needed

--- get-amalgamated-libmdbx.sh
+++ get-amalgamated-libmdbx.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-set -e
-
-GIT_TAG='v0.11.6'
+# Configurables:
+GIT_TAG='v0.11.7'
 PKG_NAME='libmdbx'
 REPO_URL='https://gitflic.ru/project/erthink/libmdbx.git'
+WARNINGS_ARE_NOT_ERRORS='true'
 
+# Bail on error:
+set -e
+
+# Grab the source:
 rm -rf ${PKG_NAME}-src
 git clone $REPO_URL ${PKG_NAME}-src
 cd ${PKG_NAME}-src
 git checkout -b ${PKG_NAME}-${GIT_TAG} tags/${GIT_TAG}
+
+# Temporarily turn off treatment of warnings as errors (set only if needed):
+if ! [ "$WARNINGS_ARE_NOT_ERRORS" = 'true' ]; then
+	sed -e 's/-Werror//g' GNUmakefile > GNUmakefile.tmp && mv GNUmakefile.tmp GNUmakefile
+	git commit -a -m 'Turned off warnings as errors'
+fi
 
 # Build specific stuff:
 make release-assets
